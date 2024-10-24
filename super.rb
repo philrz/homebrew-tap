b05644071e25b3b52909819b1fc850afa1106605
Phil Rzewski <phil@brimdata.io>
2024-10-24 11:40:47 -0700

    wip

--- super.rb
+++ super.rb
@@ -1,7 +1,7 @@
 class Super < Formula
   desc "Query and search command file formats or SuperDB data lakes"
   homepage "https://superdb.org"
-  url "https://github.com/philrz/super/archive/727a84d.zip"
+  url "https://github.com/brimdata/super/archive/727a84d.zip"
   sha256 "c56255af9871e5752f78d06b037ce72b51ae14e520f0ff0e2c8f020b0e48e471"
   version "727a84d"
 
@@ -11,7 +11,7 @@ class Super < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"build/src").mkpath
     ln_s buildpath, buildpath/"build/src/github.com"
-    system "GOPATH=$PWD/build go install github.com/philrz/super@727a84d"
+    system "GOPATH=$PWD/build go install github.com/brimdata/super@727a84d"
     bin.install "build/bin/super"
   end
 end
