21f4af03f0c18946ae34b0d50e56c54dcec1bcb3
Phil Rzewski <phil@brimdata.io>
2025-04-10 14:38:11 -0700

    Latest super and go 1.24

--- super.rb
+++ super.rb
@@ -1,17 +1,17 @@
 class Super < Formula
   desc "Query and search data in files or SuperDB data lakes"
   homepage "https://superdb.org"
-  url "https://github.com/brimdata/super/archive/f55d7bd.zip"
-  sha256 "421c8310c440e9e0375df5e65e7886da4e0a7714a1181e29f4a1ae2d055ab84a"
-  version "f55d7bd"
+  url "https://github.com/brimdata/super/archive/da56639.zip"
+  sha256 "b44eae9937fef284a3a1cb943a450c8acd702328bfdb99431abf59978b63f367"
+  version "da56639"
 
-  depends_on "go@1.23" => :build
+  depends_on "go@1.24" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"build/src").mkpath
     ln_s buildpath, buildpath/"build/src/github.com"
-    system "GOPATH=$PWD/build go install github.com/brimdata/super/cmd/super@f55d7bd"
+    system "GOPATH=$PWD/build go install github.com/brimdata/super/cmd/super@da56639"
     bin.install "build/bin/super"
   end
 end
