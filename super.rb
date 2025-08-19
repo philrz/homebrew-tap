63db8eaa73517b1c03aee94053f647bce26bc90e
Phil Rzewski <phil@brimdata.io>
2025-08-19 12:53:57 -0700

    add SHA hash

--- super.rb
+++ super.rb
@@ -2,7 +2,7 @@ class Super < Formula
   desc "A new analytics database that fuses structured and semi-structured data"
   homepage "https://superdb.org"
   url "https://github.com/brimdata/super/archive/4130d71.zip"
-  sha256 "4130d71067170452b2f22d2dc19157cc200351fd"
+  sha256 "3310943cf50523b83c9e5d0a0107f61e03c3f3462d52ab4f48e1c7b62a44cdfa"
   version "4130d71"
 
   depends_on "go@1.24" => :build
