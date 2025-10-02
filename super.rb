0fb9523ce1336fd4c5eefb30d124cad029485df1
Phil Rzewski <phil@brimdata.io>
2025-10-02 11:48:44 -0700

    Revert "Remove super.rb Formula"
    This reverts commit 08e0a61b0051d997e6e5a24c81ab2de8d84dfdf3.

---
+++ super.rb
@@ -0,0 +1,17 @@
+class Super < Formula
+  desc "A new analytics database that fuses structured and semi-structured data"
+  homepage "https://superdb.org"
+  url "https://github.com/brimdata/super/archive/4130d71.zip"
+  sha256 "3310943cf50523b83c9e5d0a0107f61e03c3f3462d52ab4f48e1c7b62a44cdfa"
+  version "4130d71"
+
+  depends_on "go@1.24" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"build/src").mkpath
+    ln_s buildpath, buildpath/"build/src/github.com"
+    system "GOPATH=$PWD/build go install github.com/brimdata/super/cmd/super@4130d71"
+    bin.install "build/bin/super"
+  end
+end
