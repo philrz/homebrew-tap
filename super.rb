aaca95aa2d8ad19bc23a1481cdebf67cfe75474f
Phil Rzewski <phil@brimdata.io>
2025-10-02 12:09:37 -0700

    Remove Go dependency

--- super.rb
+++ super.rb
@@ -5,8 +5,6 @@ class Super < Formula
   sha256 "3310943cf50523b83c9e5d0a0107f61e03c3f3462d52ab4f48e1c7b62a44cdfa"
   version "4130d71"
 
-  depends_on "go@1.24" => :build
-
   def install
     (bin/"super").write <<~EOS
       #!/bin/bash
